857ed885e02de4d2903ebd8d5e7e313111d2f826
saksham-tiwari <sakshamt234@gmail.com>
2022-10-06 00:48:50 +0530

    added API to get data about ISS and consoled it.

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ const map = L.map("map", {
   ],
 });
 
+const apiCall = ()=>{
+  fetch("https://api.wheretheiss.at/v1/satellites/25544")
+        .then((res) => {
+          return res.json()
+          })
+          .then((data) => {
+            console.log({
+              "latitude": data.latitude,
+              "longitude": data.longitude,
+              "altitude": data.altitude,
+              "velocity": data.velocity,
+              "units": "kilometers"
+          })           
+            })
+            .catch((error) => {
+                console.log(error)
+            });  
+}
+
+apiCall();
+
 map.fitWorld();
 
 L.tileLayer(
